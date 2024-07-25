4bedb961207ddd324a9eb977156f963db94e6e75
Rana <sanchitrana128@gmail.com>
2024-07-26 03:33:57 +0530

    constants config

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const USERS_API = "http://localhost:5000/users"
-export const POSTS_API = "http://localhost:5000/posts"
-export const AUTH_API = "http://localhost:5000/auth"
-export const ASSETS_API = "http://localhost:5000/assets"
+const BASE_URL= import.meta.env.VITE_NODE_ENV ==="production" ? "https://socialmedia-app-fszt.onrender.com":"http://localhost:5000"
+export const USERS_API = `${BASE_URL}/users`
+export const POSTS_API = `${BASE_URL}/posts`
+export const AUTH_API = `${BASE_URL}/auth`
+export const ASSETS_API = `${BASE_URL}/assets`
