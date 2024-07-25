3e38d7eaf6e3a2f1392950c5799c9edaee31e729
Rana <sanchitrana128@gmail.com>
2024-07-26 02:03:05 +0530

    testing routing

--- server/index.js
+++ server/index.js
@@ -59,7 +59,10 @@ app.use("/posts",postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 5000;
 
-
+console.log(process.env.NODE_ENV)
+app.get("/test", (req, res) => {
+  res.send("Test route is working");
+});
 // ************DEPLOY***********
 const __dirname1 = path.resolve()
 if(process.env.NODE_ENV === "production"){
